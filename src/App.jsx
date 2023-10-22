fc4cd6efe5a397aa5819ac71b241043f26695465
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-10-22 16:55:31 +0200

    Added persistor and delete coments

--- src/App.jsx
+++ src/App.jsx
@@ -5,50 +5,6 @@ import { Filter } from 'components/Filter/Filter';
 import css from './App.module.css';
 
 export const App = () => {
-  // const [contacts, setContacts] = useState(
-  //   () => JSON.parse(localStorage.getItem('contacts')) ?? []
-  // );
-  // const [filter, setFilter] = useState('');
-
-  //! const ContactsData = {
-  //   contacts,
-  //   filter,
-  // };
-
-  //! useEffect(() => {
-  //   const stringifiedContacts = JSON.stringify(contacts);
-  //   localStorage.setItem('contacts', stringifiedContacts);
-  // }, [contacts]);
-
-  //? const handleFilterInputChange = event => {
-  //?   setFilter(event.target.value);
-  //? };
-
-  //? const handleAddContact = contactData => {
-  //?   const hasContactDuplicate = contacts.some(
-  //?     contact => contact.name.toLowerCase() === contactData.name.toLowerCase()
-  //?   );
-
-  //?   if (hasContactDuplicate) {
-  //?     alert(`${contactData.name} is already in contacts`);
-  //?     return;
-  //?   }
-
-  //?   setContacts(prevState => {
-  //?     return [...prevState, contactData];
-  //?   });
-  //? };
-
-  //? const onDeleteBtnClick = contactId => {
-  //?   setContacts(prevState => {
-  //?     return prevState.filter(contact => contact.id !== contactId);
-  //?   });
-  //? };
-
-  //? const filterContactsArr = contacts.filter(contact => {
-  //?   return contact.name.toLowerCase().includes(filter.toLowerCase());
-  //? });
-
   return (
     <div className={css.wrapper}>
       <h1 className={css.formTitle}>Phonebook</h1>
